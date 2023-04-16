c9d576a498f4e56244b795aa125c8decc70584c1
Howard Do <howarddo2208@gmail.com>
2023-04-16 17:36:59 +0700

    config luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,7 +1,11 @@
 return {
-    "L3MON4D3/LuaSnip",
-    -- follow latest release.
-    version = "<CurrentMajor>.*",
-    -- install jsregexp (optional!).
-    -- build = "make install_jsregexp"
+    {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "<CurrentMajor>.*",
+        -- install jsregexp (optional!).
+        -- build = "make install_jsregexp"
+
+    },
+    { 'saadparwaiz1/cmp_luasnip' }
 }
