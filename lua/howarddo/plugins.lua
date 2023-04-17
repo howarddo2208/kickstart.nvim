e011d86bc9f2080457e30a53d396edaf22526456
Howard Do <howarddo2208@gmail.com>
2023-04-17 17:51:23 +0700

    transparent theme, remove one dark + addd rust formater

--- lua/howarddo/plugins.lua
+++ lua/howarddo/plugins.lua
@@ -28,19 +28,13 @@ return {
     end,
   },
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    -- priority = 1000,
-    -- -- @error: if I config transparent like ThePrimeagen, lualine will fail
-    -- config = function()
-    --     vim.cmd.colorscheme 'onedark'
-    -- end,
-  },
-  {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
     config = function()
+      require('catppuccin').setup {
+        transparent_background = true,
+      }
       vim.cmd.colorscheme 'catppuccin'
     end,
   },
