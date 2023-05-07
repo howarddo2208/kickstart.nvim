dcd9473e87f1ec134e090efcce7d83533403e147
Howard Do <howarddo2208@gmail.com>
2023-05-07 20:54:23 +0700

    auto create folder on buffer save

--- lua/howarddo/autocommands.lua
+++ lua/howarddo/autocommands.lua
@@ -13,3 +13,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+-- auto create folder when saving buffer incase the folder doesn't exist
+-- reference: https://jdhao.github.io/2022/08/21/you-do-not-need-a-plugin-for-this/
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = "*",
+  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+  callback = function(ctx)
+    local dir = vim.fn.fnamemodify(ctx.file, ":p:h")
+    vim.fn.mkdir(dir, "p")
+  end
+})
