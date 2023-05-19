3cd1c21856238baac8fb8028fb2a3b8b4fd42a63
Howard Do <howarddo2208@gmail.com>
2023-05-20 01:58:16 +0700

    add markdown snippets, config ImgPaste

--- after/plugin/clipboard-image.lua
+++ after/plugin/clipboard-image.lua
@@ -14,8 +14,8 @@ require 'clipboard-image'.setup {
   -- If you're uncertain what to name your field to, you can run `lua print(vim.bo.filetype)`
   -- Missing options from `markdown` field will be replaced by options from `default` field
   markdown = {
-    img_dir = { "src", "assets", "img" }, -- Use table for nested dir (New feature form PR #20)
-    img_dir_txt = "/assets/img",
+    img_dir = { "content", "assets" }, -- Use table for nested dir (New feature form PR #20)
+    img_dir_txt = "/assets",
     img_handler = function(img)         -- New feature from PR #22
       local script = string.format('./image_compressor.sh "%s"', img.path)
       os.execute(script)
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -9,7 +9,7 @@ require('nvim-treesitter.configs').setup {
     enable_autocmd = false,
   },
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
